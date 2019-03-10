77eabd13fb39fb50d6ccad6adcb1ba0266304f10
Dean Roskell <dean@primevr.io>
2019-03-09 23:50:00 -0700

    added json data pages

---
+++ js/cardpanel.js
@@ -0,0 +1,52 @@
+
+// Add class card-link to any button element
+// Add class card-panel to any content element
+
+class CardPanels {
+    constructor() {
+        this.cards = document.querySelectorAll( '.card-link' );
+        this.panels = document.querySelectorAll( '.card-panel' );
+        this.init();
+    }
+
+    init() {
+        this.setCardLinks();
+    }
+
+    setCardLinks() {
+        this.cards.forEach( card => {
+            card.addEventListener( 'click', e => {
+                this.selectCard( card.dataset.target );
+                console.log( card.dataset.target );
+                // Update URI
+                const h = window.location.hash;
+                const hash = ( h.includes("?") ) ? h.substr(0, h.indexOf("?") ) : h;
+                const uri = `${hash}${this.itemURI}${card.dataset.target}`;
+                // this.pushState( uri, card.dataset.target );
+                // window.history.pushState( { id: `#${uri}` }, 'Dean Roskell', `${uri}` );
+                window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+                // this.setPageMetaData();
+
+            }, false );
+        });
+    }
+
+    selectCard( cardId ) {
+        const cards = Array.from( this.cards );
+        const card = cards.find( item => item.id === `card-${cardId}` );
+        if ( typeof card === 'undefined' ) return;
+        this.unsetAllCards();
+        card.classList.add( 'selected' );
+        this.hideAllCardPages();
+        // Unhide selected content
+        document.querySelector( `#content-${cardId}` ).classList.remove( 'hide' );
+    }
+
+    unsetAllCards() {
+        this.cards.forEach( card => card.classList.remove('selected') );        // Remove highlight
+    }
+
+    hideAllCardPages() {
+        this.panels.forEach( c => c.classList.add( 'hide') );                   // Hide all content
+    }
+}
--- js/site.js
+++ js/site.js
@@ -1,33 +1,7 @@
 
 document.addEventListener( 'DOMContentLoaded', () => {
 
-    function setCardLinks() {
-        this.nav.cards.all.forEach( card => {
-            card.addEventListener( 'click', e => {
-                this.selectCard( card.dataset.target );
-                // Update URI
-                const h = window.location.hash;
-                const hash = ( h.includes("?") ) ? h.substr(0, h.indexOf("?") ) : h;
-                const uri = `${hash}${this.itemURI}${card.dataset.target}`;
-                this.pushState( uri, card.dataset.target );
-                // window.history.pushState( { id: `#${uri}` }, 'Dean Roskell', `${uri}` );
-                window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
-                this.setPageMetaData();
-
-            }, false );
-        });
-    }
-
-    function selectCard( cardId, tabId = 'all',  ) {
-        const cards = Array.from( s.nav.cards[tabId] );
-        const card = cards.find( item => item.id === `card-${cardId}` );
-        if ( typeof card === 'undefined' ) return;
-        this.unsetAllCards();
-        card.classList.add( 'selected' );
-        this.hideAllContentPages();
-        // Unhide selected content
-        document.querySelector( `#content-${cardId}` ).classList.remove( 'hide' );
-    }
+    new CardPanels();
 
     // init Fade in on Screen
     const fader = new FadeInOnScreen();
