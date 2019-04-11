dcefef0911bb4a8e2ec1349388c71f46c5b3484f
Dean Roskell <dean@primevr.io>
2019-04-10 22:33:47 -0600

    updated index to services page update

--- js/site.js
+++ js/site.js
@@ -8,4 +8,6 @@ document.addEventListener( 'DOMContentLoaded', function() {
     sideNavBtn.addEventListener( 'click', () => {
         sideNav[0].open();
     }, false );
+
+    AOS.init();
 }, false );
